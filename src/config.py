dc2b4aa4421e9a6077bc56a2fb1a7b24806f818a
giadaL <gl99.giada@gmail.com>
2022-04-12 15:00:20 +0200

    smtp data configuration

---
+++ src/config.py
@@ -0,0 +1,18 @@
+import json
+
+
+class UseDotNotation(dict):
+    __getattr__ = dict.get
+    __setattr__ = dict.__setattr__
+    __delattr__ = dict.__delattr__
+
+
+def _load_json(file_path: str) -> dict:
+    with open(file_path) as file:
+        config_parameters = file.read()
+
+    config_default = json.loads(config_parameters)
+    return UseDotNotation(config_default)
+
+
+SMTP_CONFIG = _load_json("config/smtp_config.json")
